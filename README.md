17c1e288a59cdf0ac3d5457e8329764664d360bc
malhill <74620676+malhill@users.noreply.github.com>
2021-01-09 23:46:32 -0500

    added notes to js and readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Password-Security
+
+understanding the process of creating variables, giving them a function, then calling the function!
+
+used console.log frequently to test function
+
+completed most of the js code, most pieces in place, referenced other password generators. 
+
+after using office hours I learned that I was using the 'return function wrong'. Being too detailed I added too many 'statements' and in other cases needed to be specific in the 'functions' of the generator. 
+
+27 insertions 16 deletions for my final edition of the homework
--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 // // THEN I select which criteria to include in the password
 alert("Please, select ''Generate Password'' for your password");
 
+//creating the arrays!
 let passwordString="";
 let caseArray=[];
 let lowerCaseChar=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -14,6 +15,7 @@ let specialChar=["!","@","#","$","%","^","&","*","(",")","{","}","|","[","]",";"
 
 // // WHEN I click the button to generate a password
 // // THEN I am presented with a series of prompts for password criteria
+
 function generatePassword() {
   let useLowerCase=confirm("Do you want to include lowercase characters?");
   let useUpperCase=confirm("Do you want to include uppercase characters?");
@@ -21,16 +23,26 @@ function generatePassword() {
   let useSpecial=confirm("Do you want to include special characters?");
   let useLength = parseInt(prompt("How long do you want your password to be?"));
 
+  // WHEN prompted for password criteria
+  // THEN I select which criteria to include in the password
   if (useLowerCase == false && useUpperCase == false && useNumeric == false && useSpecial == false) {
+    // WHEN I answer each prompt
+    // THEN my input should be validated and at least one character type should be selected
     alert("Please select one type of character");
     return;
   }
-  // check to make sure that line 28 is only a number
+  // WHEN prompted for the length of the password
+  // THEN I choose a length of at least 8 characters and no more than 128 characters
+  
   if (useLength < 8 || useLength > 128 || isNaN(useLength)) {
+    // WHEN I answer each prompt
+    // THEN my input should be validated and at least one character type should be selected
     alert("Please select length 8 or more, but less than 128")
+    // check to make sure that line 28 is only a number
     console.log(useLength);
   }
-
+  // WHEN prompted for character types to include in the password
+  // THEN I choose lowercase, uppercase, numeric, and/or special characters
   caseArray = [];
   if(lowerCaseChar) {
     caseArray = caseArray.concat(lowerCaseChar);
@@ -54,40 +66,49 @@ function generatePassword() {
     //console.log(caseArray[i]);
     var random = Math.floor(Math.random() * caseArray.length);
     var letter = caseArray[random];
-    console.log(letter);
+    //console.log(letter);
     passwordString = passwordString + letter;
-  }
-  //console.log(caseArray.length);
-  
-  return passwordString; // exit the function with value - password
 
-
-  //for (var i=0; i < useLength; i++){
+    //[a comparison to above 'for' statement] learned to add the function before the 'for' function. then utilizing the right function (caseArray > useLength)
+    //for (var i=0; i < useLength; i++){
     //var random= Math.floor(Math.random() * useLength.length);
     //console.log(random);
     //}
+  }
+  //testing if the caseArray is functioning
+  //console.log(caseArray.length);
+  return passwordString; // exit the function with value - password
+
+
+  
 }
 
-// Assignment Code
+// Assignment Code. Given by homework
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password input. Given by homework
 function writePassword() {
   var passwordString = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = passwordString;
 }
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
 generateBtn.addEventListener('click', writePassword);
 
 
-
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page
 function UserInput(ps) {
   document.getElementById("password").textContent = ps;
 }
 
-console.log("peter");
+// testing if all of the code will finish. Will utilize this more in my own code!
+//console.log("peter");
 
+
+//Homework Criteria:
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
 // THEN I am presented with a series of prompts for password criteria
